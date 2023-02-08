f40a2f54730f911ca765a515ac23b88265caa44b
ceghap <hello@ceghap.com>
2023-02-08 23:02:51 +0800

    setup font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,5 +5,14 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
