02361cb611fef755cc3b0a9c4dbe5ec5145e6a5a
ceghap <hello@ceghap.com>
2023-02-08 22:58:45 +0800

    - Add navbar - Add public layout

--- src/components/layouts/PublicLayout.tsx
+++ src/components/layouts/PublicLayout.tsx
@@ -4,6 +4,7 @@ import Navbar from "../Navbar";
 const PrivateLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
+      <Navbar />
       <div className="format p-4">{children}</div>
     </div>
   );
