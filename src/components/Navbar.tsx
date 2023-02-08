cace99253986481d959bd555fa571608d926e974
ceghap <hello@ceghap.com>
2023-02-08 23:03:46 +0800

    add border in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="rounded border-gray-200 bg-white px-2 py-2.5 dark:bg-gray-900 sm:px-4">
+    <nav className="rounded border-b border-gray-200 bg-white px-2 py-2.5 dark:bg-gray-900 sm:px-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         <a href="/" className="flex items-center">
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
