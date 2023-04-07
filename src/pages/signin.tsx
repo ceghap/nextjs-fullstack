1902dd9c72bf71ee41ff23b6d5f77e94d74b8da8
ceghap <hello@ceghap.com>
2023-04-07 16:20:30 +0800

    fix width on signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -77,7 +77,7 @@ export default function Signin() {
       </Head>
       {isSubmitting && <Spinner />}
 
-      <main className="flex w-screen justify-center p-4">
+      <main className="flex justify-center p-4">
         <section className="w-full dark:bg-gray-900">
           <div className="mx-auto flex h-full flex-col items-center justify-center px-6 py-8 lg:py-0">
             <div className="w-full rounded-lg bg-white sm:max-w-md md:mt-0 xl:p-0">
