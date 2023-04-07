153f671b05826c7b02a94b75313d028576ab76fc
ceghap <hello@ceghap.com>
2023-04-07 16:39:37 +0800

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,14 +14,14 @@ const Footer = () => {
         <p className="my-6 text-gray-500 dark:text-gray-400">
           Just another nextjs fullstack template.
         </p>
-        <ul className="mb-3 flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
+        <ul className="mb-3 flex flex-wrap items-center justify-center space-x-2 text-sm text-gray-500 dark:text-white">
           <li>
-            <Link href="/about" className="mr-4 hover:underline md:mr-6 ">
+            <Link href="/about" className="hover:underline">
               About
             </Link>
           </li>
           <li>
-            <Link href="/contact" className="mr-4 hover:underline md:mr-6">
+            <Link href="/contact" className="hover:underline">
               Contact
             </Link>
           </li>
@@ -29,7 +29,7 @@ const Footer = () => {
         <div className="mb-3 flex justify-center space-x-2">
           <Link
             href="https://github.com/ceghap/nextjs-fullstack"
-            className="text-xl text-gray-500"
+            className="text-4xl text-gray-500"
           >
             <ImGithub />
           </Link>
