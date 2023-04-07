c1b46a54dc26b207722de61c97e823a9a95b1cae
ceghap <hello@ceghap.com>
2023-04-07 16:21:21 +0800

    change button name

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -175,7 +175,7 @@ export default function Signin() {
                     type="submit"
                     className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:opacity-75 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
                   >
-                    {watch("haspassword") ? "Sign in" : "Send the magic link"}
+                    {watch("haspassword") ? "Sign in" : "Send me magic link"}
                   </button>
                 </form>
 
