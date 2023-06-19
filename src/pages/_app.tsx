23a42b5cbf9381197b4533f2dec6b95c1781cb2b
Ashraf Latif <hello@ceghap.com>
2023-06-19 14:57:11 +0800

    remove @next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
